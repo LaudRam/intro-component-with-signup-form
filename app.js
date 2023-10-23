f3f495d33af26f9da602a00a9e21fabf94bfb06c
LaudRam <laudiciaramasenya@gmail.com>
2023-10-23 20:56:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 console.log("Testing!!!");
+
+function handleSubmit(event) {
+  const isValid = event.target.checkValidity();
+
+  if (isValid === false) {
+    alert("WTF!!!");
+  }
+}
+document.querySelector("button").addEventListener("click", handleSubmit);
